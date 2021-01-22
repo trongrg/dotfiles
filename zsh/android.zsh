655568d0411632e10c2af76b3b0ba980d2ffc45a
Trong Tran <trong.tran@sstechvn.com>
2021-01-22 09:28:02 +0700

    Remove android path, use asdf

--- zsh/android.zsh
+++
@@ -1,3 +0,0 @@
-export JAVA_HOME='/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home'
-export ANDROID_HOME=/Users/trongtran/Library/Android/sdk
-export ANDROID_SDK_ROOT=/Users/trongtran/Library/Android/sdk
